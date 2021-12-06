579b40869080e611eac36fde3446e4469aba41b5
wongzongbo <wongzonbo@163.com>
2021-12-06 10:18:41 +0800

    error

--- src/main/java/com/demo/ForkDemo.java
+++ src/main/java/com/demo/ForkDemo.java
@@ -3,5 +3,6 @@ package com.demo;
 public class ForkDemo {
     public static void main(String[] args) {
         System.out.println("fork demo--------");
+        System.out.println("error commit ");
     }
 }
